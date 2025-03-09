2a10800584a03a28f3a2363bb0ebfdde0e14d772
webcoderz <19884161+webcoderz@users.noreply.github.com>
2025-03-09 15:59:39 -0400

    code reorg

--- iointel/__init__.py
+++ iointel/__init__.py
@@ -1,4 +1,4 @@
-from .src.magic import (
+from .src.utilities.magic import (
     UNUSED as UNUSED,
 )  # this performs some magic to hide controlflow warning
 from .src.agents import Agent
@@ -7,7 +7,7 @@ from .src.agents import Agent
 from .src.memory import Memory
 from .src.workflow import Workflow, run_agents
 from .src.agent_methods.data_models.datamodels import PersonaConfig
-from .src.handlers import AsyncLoggingHandler, LoggingHandler
+from .src.utilities.handlers import AsyncLoggingHandler, LoggingHandler
 from .client import client
 
 
