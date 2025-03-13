b32f03dc493c417e212116eb54bf9645f6383f84
webcoderz <19884161+webcoderz@users.noreply.github.com>
2025-03-13 14:42:54 -0400

    fix runners

--- iointel/__init__.py
+++ iointel/__init__.py
@@ -3,7 +3,8 @@ from .src.agents import Agent
 
 # from .src.memory import AsyncMemory, AsyncPostgresMemoryProvider, Memory, PostgresMemoryProvider
 from .src.memory import Memory
-from .src.workflow import Workflow, run_agents
+from .src.workflow import Workflow, 
+from .src.utilities.runners import run_agents
 from .src.agent_methods.data_models.datamodels import PersonaConfig
 from .src.utilities.handlers import AsyncLoggingHandler, LoggingHandler
 from .client import client
