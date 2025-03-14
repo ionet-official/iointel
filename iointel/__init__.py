061f2986bccc9cbb53cd3492cc336e515461a960
webcoderz <19884161+webcoderz@users.noreply.github.com>
2025-03-14 01:24:04 -0400

    forgot task registration and tool registration import

--- iointel/__init__.py
+++ iointel/__init__.py
@@ -6,6 +6,7 @@ from .src.memory import Memory
 from .src.workflow import Workflow
 from .src.utilities.runners import run_agents, run_agents_async
 from .src.agent_methods.data_models.datamodels import PersonaConfig
+from .src.utilities.decorators import register_custom_task, register_tool
 
 from .src.utilities.decorators import register_custom_task, register_tool
 from .src.utilities.runners import run_agents, run_agents_async
@@ -28,6 +29,8 @@ __all__ = [
     "register_tool",
     "run_agents",
     "run_agents_async",
+    "register_custom_task",
+    "register_tool",
     "PersonaConfig",
     ###code parsers###
     "PythonModule",
