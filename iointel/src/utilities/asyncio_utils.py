ed3827a34127c2cee208b80b1643d7dc1fc11a8a
Ivan Smirnov <ismirn0ff@yandex.ru>
2025-04-11 14:18:52 +0400

    Implement thread pool for async

--- iointel/src/utilities/asyncio_utils.py
+++ iointel/src/utilities/asyncio_utils.py
@@ -3,23 +3,35 @@ import inspect
 import threading
 from typing import Coroutine, Any
 
-_loop = asyncio.new_event_loop()
-_thr = threading.Thread(target=_loop.run_forever, name="Async Runner", daemon=True)
-_in_thread = False
+_thread_pool_size = 10
+_loops = [asyncio.new_event_loop() for _ in range(_thread_pool_size)]
+_threads = [
+    threading.Thread(target=_loops[i].run_forever, name="Async Runner", daemon=True)
+    for i in range(_thread_pool_size)
+]
+_in_thread = [False] * _thread_pool_size
 
 
 # This will block the calling thread until the coroutine is finished.
 # Any exception that occurs in the coroutine is raised in the caller
 def run_async(coro: Coroutine):
-    if not _thr.is_alive():
-        _thr.start()
-    global _in_thread
-    if _in_thread:
-        raise Exception("You are already inside run_async thread. It's a loop!")
-    _in_thread = True
-    future = asyncio.run_coroutine_threadsafe(coro, _loop)
+    global _loops, _threads, _in_thread
+    available_thread_id = None
+    for i in range(_thread_pool_size):
+        if not _in_thread[i]:
+            available_thread_id = i
+            break
+    if available_thread_id is None:
+        raise Exception(
+            "All 10 threads are in use. You are probably doing it wrong."
+            "This function is supposed to run async functions from sync context."
+        )
+    _in_thread[available_thread_id] = True
+    if not _threads[available_thread_id].is_alive():
+        _threads[available_thread_id].start()
+    future = asyncio.run_coroutine_threadsafe(coro, _loops[available_thread_id])
     result = future.result()
-    _in_thread = False
+    _in_thread[available_thread_id] = False
     return result
 
 
