430a0f6c430b64dd75ea46bb0b05a531745657f5
carlos <carlos@io.net>
2025-06-05 15:24:11 -0300

    Change pattern

--- iointel/src/agent_methods/tools/agno/email.py
+++ iointel/src/agent_methods/tools/agno/email.py
@@ -1,34 +1,13 @@
-from functools import wraps
-from typing import Optional
-from pydantic import BaseModel, Field
 from agno.tools.email import EmailTools as AgnoEmailTools
 
-from ..utils import register_tool
-from .common import DisableAgnoRegistryMixin
+from .common import make_base, wrap_tool
 
 
-class Email(BaseModel, DisableAgnoRegistryMixin, AgnoEmailTools):
-    receiver_email: Optional[str] = Field(default=None)
-    sender_name: Optional[str] = Field(default=None)
-    sender_email: Optional[str] = Field(default=None)
-    sender_passkey: Optional[str] = Field(default=None)
+class Email(make_base(AgnoEmailTools)):
+    def _get_tool(self):
+        return self.Inner()
 
-    def __init__(
-        self,
-        receiver_email: Optional[str] = None,
-        sender_name: Optional[str] = None,
-        sender_email: Optional[str] = None,
-        sender_passkey: Optional[str] = None,
-    ):
-        super().__init__(
-            receiver_email=receiver_email,
-            sender_name=sender_name,
-            sender_email=sender_email,
-            sender_passkey=sender_passkey,
-        )
-
-    @register_tool(name="email_user")
-    @wraps(AgnoEmailTools.email_user)
+    @wrap_tool("email_user", AgnoEmailTools.email_user)
     def email_user(self, subject: str, body: str) -> str:
         """
         Send an email message to the user.
