9d528c009029ec800b6b42e114a4fee295ed5a34
carlos <carlos@io.net>
2025-06-17 15:41:39 -0300

    Add fields

--- iointel/src/agent_methods/tools/agno/knowledge.py
+++ iointel/src/agent_methods/tools/agno/knowledge.py
@@ -1,17 +1,25 @@
-from typing import Union
+from typing import Optional, Union
 from agno.tools.knowledge import KnowledgeTools as AgnoKnowledgeTools
 from .common import make_base, wrap_tool
 from agno.agent.agent import Agent
 from agno.team.team import Team
+from agno.knowledge.agent import AgentKnowledge
+from pydantic import Field
 
 
 class Knowledge(make_base(AgnoKnowledgeTools)):
+    knowledge: AgentKnowledge
+    instructions: Optional[str] = Field(default=None, frozen=True)
+    add_instructions: bool = Field(default=True, frozen=True)
+    add_few_shot: bool = Field(default=False, frozen=True)
+    few_shot_examples: Optional[str] = Field(default=None, frozen=True)
+
     def _get_tool(self):
         return self.Inner(
             knowledge=self.knowledge_,
-            think=self.think_,
-            search=self.search_,
-            analyze=self.analyze,
+            think=True,
+            search=True,
+            analyze=True,
             instructions=self.instructions_,
             add_instructions=self.add_instructions_,
             add_few_shot=self.add_few_shot_,
