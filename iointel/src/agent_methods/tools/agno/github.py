67d63dd785004e1541212f60ff7fdb49fb965e6e
carlos <carlos@io.net>
2025-06-17 16:48:52 -0300

    Remove comment

--- iointel/src/agent_methods/tools/agno/github.py
+++ iointel/src/agent_methods/tools/agno/github.py
@@ -6,7 +6,6 @@ from pydantic import Field
 
 
 class Github(make_base(AgnoGithubTools)):
-    # test
     access_token: Optional[str] = Field(default=None, frozen=True)
     base_url: Optional[str] = Field(default=None, frozen=True)
 
