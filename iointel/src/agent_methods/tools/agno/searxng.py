9b865879de09c25b8029e19afc51238f1778b4d7
carlos <carlos@io.net>
2025-06-18 12:42:03 -0300

    Multiple fixes

--- iointel/src/agent_methods/tools/agno/searxng.py
+++ iointel/src/agent_methods/tools/agno/searxng.py
@@ -25,32 +25,32 @@ class Se(make_base(AgnoSearxng)):
 
     @wrap_tool("agno__se__search", AgnoSearxng.search)
     def search(self, query: str, max_results: int = 5) -> str:
-        return self.search(self, query, max_results)
+        return self.search(query, max_results)
 
     @wrap_tool("agno__se__image_search", AgnoSearxng.image_search)
     def image_search(self, query: str, max_results: int = 5) -> str:
-        return self.image_search(self, query, max_results)
+        return self.image_search(query, max_results)
 
     @wrap_tool("agno__se__it_search", AgnoSearxng.it_search)
     def it_search(self, query: str, max_results: int = 5) -> str:
-        return self.it_search(self, query, max_results)
+        return self.it_search(query, max_results)
 
     @wrap_tool("agno__se__map_search", AgnoSearxng.map_search)
     def map_search(self, query: str, max_results: int = 5) -> str:
-        return self.map_search(self, query, max_results)
+        return self.map_search(query, max_results)
 
     @wrap_tool("agno__se__music_search", AgnoSearxng.music_search)
     def music_search(self, query: str, max_results: int = 5) -> str:
-        return self.music_search(self, query, max_results)
+        return self.music_search(query, max_results)
 
     @wrap_tool("agno__se__news_search", AgnoSearxng.news_search)
     def news_search(self, query: str, max_results: int = 5) -> str:
-        return self.news_search(self, query, max_results)
+        return self.news_search(query, max_results)
 
     @wrap_tool("agno__se__science_search", AgnoSearxng.science_search)
     def science_search(self, query: str, max_results: int = 5) -> str:
-        return self.science_search(self, query, max_results)
+        return self.science_search(query, max_results)
 
     @wrap_tool("agno__se__video_search", AgnoSearxng.video_search)
     def video_search(self, query: str, max_results: int = 5) -> str:
-        return self.video_search(self, query, max_results)
+        return self.video_search(query, max_results)
