b5f94138585e1096d3581f50fdb67eb3a62ab90f
carlos <carlos@io.net>
2025-06-18 11:19:53 -0300

    Address comments

--- iointel/src/agent_methods/tools/agno/telegram.py
+++ iointel/src/agent_methods/tools/agno/telegram.py
@@ -1,12 +1,17 @@
+from typing import Optional, Union
 from agno.tools.telegram import TelegramTools as AgnoTelegramTools
 from .common import make_base, wrap_tool
+from pydantic import Field
 
 
 class Telegram(make_base(AgnoTelegramTools)):
+    chat_id: Union[str, int] = Field(frozen=True)
+    token: Optional[str] = Field(default=None, frozen=True)
+
     def _get_tool(self):
         return self.Inner(
-            chat_id=self.chat_id_,
-            token=self.token_,
+            chat_id=self.chat_id,
+            token=self.token,
         )
 
     @wrap_tool("agno__telegram__send_message", AgnoTelegramTools.send_message)
