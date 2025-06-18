3642b785519069302b9d071a7aa713f735e9f4c6
carlos <carlos@io.net>
2025-06-18 07:04:58 -0300

    multiple fixes

--- iointel/src/agent_methods/tools/agno/webex.py
+++ iointel/src/agent_methods/tools/agno/webex.py
@@ -1,13 +1,16 @@
+from typing import Optional
 from agno.tools.webex import WebexTools as AgnoWebexTools
 from .common import make_base, wrap_tool
 
 
 class Webex(make_base(AgnoWebexTools)):
+    access_token: Optional[str] = None
+
     def _get_tool(self):
         return self.Inner(
-            send_message=self.send_message_,
-            list_rooms=self.list_rooms,
-            access_token=self.access_token_,
+            send_message=True,
+            list_rooms=True,
+            access_token=self.access_token,
         )
 
     @wrap_tool("agno__webex__send_message", AgnoWebexTools.send_message)
