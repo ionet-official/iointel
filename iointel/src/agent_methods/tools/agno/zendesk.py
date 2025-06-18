6a89694c6ba538a2320f19cf4e87cb2d81f20ed3
carlos <carlos@io.net>
2025-06-18 06:41:52 -0300

    multiple fixes

--- iointel/src/agent_methods/tools/agno/zendesk.py
+++ iointel/src/agent_methods/tools/agno/zendesk.py
@@ -1,8 +1,13 @@
+from typing import Optional
 from agno.tools.zendesk import ZendeskTools as AgnoZendeskTools
 from .common import make_base, wrap_tool
 
 
 class Zendesk(make_base(AgnoZendeskTools)):
+    username: Optional[str] = (None,)
+    password: Optional[str] = (None,)
+    company_name: Optional[str] = (None,)
+
     def _get_tool(self):
         return self.Inner(
             username=self.username_,
@@ -12,4 +17,4 @@ class Zendesk(make_base(AgnoZendeskTools)):
 
     @wrap_tool("agno__zendesk__search_zendesk", AgnoZendeskTools.search_zendesk)
     def search_zendesk(self, search_string: str) -> str:
-        return self._tool.search_zendesk(self, search_string)
+        return self._tool.search_zendesk(search_string)
