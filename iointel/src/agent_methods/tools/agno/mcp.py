e5208213e3cb26c5250e1655a749a9dca672b05a
carlos <carlos@io.net>
2025-06-18 15:21:33 -0300

    Multiple fixes

--- iointel/src/agent_methods/tools/agno/mcp.py
+++ iointel/src/agent_methods/tools/agno/mcp.py
@@ -1,20 +1,43 @@
+from typing import Literal, Optional, Union
 from agno.tools.mcp import MCPTools as AgnoMCPTools
+from mcp import (
+    ClientSession,
+    StdioServerParameters,
+    SSEClientParams,
+    StreamableHTTPClientParams,
+)
 from .common import make_base, wrap_tool
+from pydantic import Field
 
 
 class MCP(make_base(AgnoMCPTools)):
+    command: Optional[str] = Field(default=None, frozen=True)
+    url: Optional[str] = Field(default=None, frozen=True)
+    env: Optional[dict[str, str]] = Field(default=None, frozen=True)
+    transport: Literal["stdio", "sse", "streamable-http"] = Field(
+        default="stdio", frozen=True
+    )
+    server_params: Optional[
+        Union[StdioServerParameters, SSEClientParams, StreamableHTTPClientParams]
+    ] = Field(default=None, frozen=True)
+    session: Optional[ClientSession] = Field(default=None, frozen=True)
+    timeout_seconds: int = Field(default=5, frozen=True)
+    client = Field(default=None, frozen=True)
+    include_tools: Optional[list[str]] = Field(default=None, frozen=True)
+    exclude_tools: Optional[list[str]] = Field(default=None, frozen=True)
+
     def _get_tool(self):
         return self.Inner(
-            command=self.command_,
-            url=self.url_,
-            env=self.env_,
-            transport=self.transport_,
-            server_params=self.server_params_,
-            session=self.session_,
-            timeout_seconds=self.timeout_seconds_,
-            client=self.client_,
-            include_tools=self.include_tools_,
-            exclude_tools=self.exclude_tools_,
+            command=self.command,
+            url=self.url,
+            env=self.env,
+            transport=self.transport,
+            server_params=self.server_params,
+            session=self.session,
+            timeout_seconds=self.timeout_seconds,
+            client=self.client,
+            include_tools=self.include_tools,
+            exclude_tools=self.exclude_tools,
         )
 
     @wrap_tool("agno__mcp__initialize", AgnoMCPTools.initialize)
