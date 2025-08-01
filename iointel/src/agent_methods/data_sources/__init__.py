88098e209cc84458d8c33e922ad689ca377c7d00
Alex Morrise <silkspace@users.noreply.github.com>
2025-08-01 11:50:47 +0200

    refactor: Extract data sources from tools registry
    - Create dedicated data_sources/ directory
    - Move user_input and prompt_tool to data sources
    - Implement DataSourceBase abstract class
    - Separate data source execution from tool execution
    - Clean up data source interfaces

---
+++ iointel/src/agent_methods/data_sources/__init__.py
@@ -0,0 +1,20 @@
+"""
+Data Sources Module
+==================
+
+This module contains data source implementations that provide INPUT data to workflows.
+These are NOT tools - they are input providers that feed data to agent nodes.
+
+Data sources are registered separately from tools and have a standardized interface.
+"""
+
+from .input_sources import user_input_source, prompt_source
+from .registry import register_data_source, get_data_source, list_data_sources
+
+__all__ = [
+    "user_input_source",
+    "prompt_source", 
+    "register_data_source",
+    "get_data_source",
+    "list_data_sources"
+]
